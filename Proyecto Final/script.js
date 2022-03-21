bb992aa2e06e594abd56ad7221bcc3a0df196ca3
Kryosen <gonuri91@hotmail.com>
2022-03-21 15:16:40 +0700

    Agregando operadores avanzados y guardando en storage con JSON

--- Proyecto Final/script.js
+++ Proyecto Final/script.js
@@ -115,14 +115,37 @@ function menu() {
   }
 
   function welcomePage() {
-    const nombre = document.getElementById("nombre");
-    nombre.innerHTML = `Hola <span>${prompt("Cual es tu nombre?")}</span>!`;
-    const bienvenida = document.getElementById("bienvenida");
-    bienvenida.innerText = `${
+    // GUARDANDO DATA EN LOCAL STORAGE CON JASON
+    if (localStorage.getItem("nombre") && localStorage.getItem("bienvenida")) {
+      const nombre = document.getElementById("nombre");
+      nombre.innerHTML = `Hola nuevamente <span>${JSON.parse(
+        localStorage.getItem("nombre")
+      )}</span>!`;
+      const bienvenida = document.getElementById("bienvenida");
+      bienvenida.innerText = `${JSON.parse(
+        localStorage.getItem("bienvenida")
+      )} de vuelta al mundo de Pokémon`;
+    } else {
+      const nombre = document.getElementById("nombre");
+      nombre.innerHTML = `Hola <span>${requestNombre()}</span>!`;
+      const bienvenida = document.getElementById("bienvenida");
+      bienvenida.innerText = `${requestIdentidad()} al mundo de Pokémon`;
+    }
+  }
+
+  function requestNombre() {
+    let nombre = prompt("Cual es tu nombre?");
+    localStorage.setItem("nombre", JSON.stringify(nombre));
+    return nombre;
+  }
+
+  function requestIdentidad() {
+    let identidad =
       prompt("Eres un chico o una chica?") === "chico"
         ? "Bienvenido"
-        : "Bienvenida"
-    } al mundo de Pokémon`;
+        : "Bienvenida";
+    localStorage.setItem("bienvenida", JSON.stringify(identidad));
+    return identidad;
   }
 
   function options() {
